f7d511bfb351b4e210d889bc08b81b1a02a2e29f
vidia <baetrisvidia@gmail.com>
2024-12-18 16:36:00 +0700

    fix error

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const Joi = require('joi')
-const app = express()
-const port = 8080
-const cors = require ("cors");
-require('dotenv').config()
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
+const userRouter = require("./routers/users.router");
+const transactionRouter = require("./routers/transactions.router");
+const globalErrorHandler = require("./middlewares/error.middleware");
 
-const userRouter = require('./routers/users.routers');
-
-app.use(cors())
-app.use(bodyParser.json())
+const app = express();
+const port = process.env.APP_PORT;
+app.use(cors());
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-)
+);
 
-app.use(userRouter)
+app.use(userRouter);
+app.use(transactionRouter);
+
+app.use(globalErrorHandler);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
