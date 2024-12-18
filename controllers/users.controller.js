aababfe8849424d04d0456614d1bb2d6dff98164
vidia <baetrisvidia@gmail.com>
2024-12-18 16:32:47 +0700

    update error

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,6 +1,11 @@
 const Joi = require("joi");
-const userService = require("../services/users.services");
-const { UserReponse } = require("../dt/userResponse");
+const userService = require("../services/users.service");
+const { UserResponse } = require("../dto/userResponse");
+const {
+  UserAlreadyExistsError,
+  AuthenticationError,
+  NotFoundError,
+} = require("../dto/customErrors");
 
 const registerSchema = Joi.object({
   email: Joi.string().email().required(),
@@ -11,58 +16,60 @@ const registerSchema = Joi.object({
 });
 
 const loginSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().required()
-  });
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
 
+const getUserById = async (req, res, next) => {
+  try {
+    const { id } = req.user;
+    const user = await userService.getUserById(Number(id));
+    res.status(200).json({ data: new UserResponse(user) });
+  } catch (error) {
+    if (error instanceof NotFoundError) {
+      return res.status(error.status).json({ error: error.message });
+    }
+    next(error);
+  }
+};
 
-
-const createUser = async (req, res) => {
+const createUser = async (req, res, next) => {
   try {
     const { error, value } = registerSchema.validate(req.body);
 
     if (error) {
-      return res.status(400).json({ error: error?.message });
+      return res.status(400).json({ error: error.message });
     }
+
     const user = await userService.createUser(value);
-    res.status(201).json({data: new UserReponse(user)});
+    res.status(201).json({ data: new UserResponse(user) });
   } catch (error) {
-    res.status(error.statusCode || 500).json({ error: error?.message });
+    if (error instanceof UserAlreadyExistsError) {
+      return res.status(error.status).json({ error: error.message });
+    }
+    next(error); 
   }
 };
 
-const getUserById =async (req, res) => {
-    try{
-        const {id} = req.user;
-        const user = await userService.getUserById(Number(id));
-        res.status(200).json({data: new UserReponse(user)});
-    } catch (error){
-        if(error?.message === "user not found"){
-            return res.status(404).json({error: error?.message});
-        }
-        res.status(error.status.statusCode||500).json({error:error?.message});
-    }
-}
+const login = async (req, res, next) => {
+  try {
+    const { error, value } = loginSchema.validate(req.body);
 
-const login = async (req, res) => {
-    try {
-      const { error, value } = loginSchema.validate(req.body);
-  
-      if (error) {
-        return res.status(400).json({ error: error.message });
-      }
-  
-      const token = await userService.login(value);
-      res.status(200).json({ data: { token: token } });
-    } catch (error) {
-      if (error?.message === "404") {
-        return res.status(404).json({ message: "user doesn't exist" });
-      }
-  
-      if (error?.message === "401") {
-        return res.status(404).json({ message: "email or password not valid" });
-      }
-      res.status(500).json({ message: error?.message });
+    if (error) {
+      return res.status(400).json({ error: error.message });
     }
-  };
-  module.exports = { createUser, getUserById, login };
+
+    const token = await userService.login(value);
+    res.status(200).json({ data: { token } });
+  } catch (error) {
+    if (error instanceof AuthenticationError) {
+      return res.status(error.status).json({ error: error.message });
+    }
+    if (error instanceof NotFoundError) {
+      return res.status(error.status).json({ error: error.message });
+    }
+    next(error);
+  }
+};
+
+module.exports = { createUser, getUserById, login };
