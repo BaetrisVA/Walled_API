3ceb900a5f7f059b741d3d6e7c6051b15ccac552
vidia <baetrisvidia@gmail.com>
2024-12-18 16:23:44 +0700

    change url in db js

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  connectionString: "postgresql://neondb_owner:cDkUwGAVi9g8@ep-crimson-mouse-a1jr4iqk.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
+  connectionString: process.env.DATABASE_URL
 });
 
 module.exports = pool;
